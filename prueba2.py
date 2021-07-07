cd4cc93ab2b11fec9b67fc5e23c15ed2652384f6
Luiggy Macias <ferrinluiggy@gmail.com>
2021-07-07 08:03:22 -0500

    updated djdjdj

--- prueba2.py
+++ prueba2.py
@@ -9,3 +9,11 @@ print(a)
 #comentario hecho en el remoto
 #otro comentario hecho en el servidor
 #chao mundo
+A = [6, 7, 8, 9, 10, 11, 12]
+subset_of_A = [6, 9, 12]; # the subset of A
+
+
+result = [a for a in A if a not in subset_of_A]
+print(result)
+result2 = set(A)- set(subset_of_A)
+print(result2)
